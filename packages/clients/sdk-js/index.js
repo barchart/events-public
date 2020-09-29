24a956e447f0f4a5ee093417546ca7c1a48f3612
Igor Losev <igor.losev.it@gmail.com>
2020-09-29 15:52:54 +0200

    WIP

--- packages/clients/sdk-js/index.js
+++ packages/clients/sdk-js/index.js
@@ -1,2 +1,12 @@
 // Minor 1.1.0 change
 // Patch 1.1.1 change
+
+// Patch 1.1.2 change
+
+const is = require('@barchart/common-js/lang/is');
+
+module.exports = (() => {
+	'use strict';
+
+	return is;
+})();
