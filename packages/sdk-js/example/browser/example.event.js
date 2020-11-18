a5745f3e962907263a9699740820caa11ea3241d
Bryan <bryan@Bryans-MacBook-Pro-2.local>
2020-11-18 12:07:11 -0700

    Release. Bump version number 3.0.1

--- packages/sdk-js/example/browser/example.event.js
+++ packages/sdk-js/example/browser/example.event.js
@@ -22624,7 +22624,7 @@ module.exports = (() => {
   'use strict';
 
   return {
-    version: '3.0.0'
+    version: '3.0.1'
   };
 })();
 
--- packages/sdk-js/lib/index.js
+++ packages/sdk-js/lib/index.js
@@ -2,6 +2,6 @@ module.exports = (() => {
 	'use strict';
 
 	return {
-		version: '3.0.0'
+		version: '3.0.1'
 	};
 })();
