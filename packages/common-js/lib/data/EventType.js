f85c94ea6c8fc73776e642f4d01b5f3ec7368c47
Bryan Ingle <bryaningle@gmail.com>
2021-05-03 10:35:26 -0600

    Remove `alertId` attribute from `EventType.ALERT_DELETED_ALL` definition

--- packages/common-js/lib/data/EventType.js
+++ packages/common-js/lib/data/EventType.js
@@ -316,7 +316,7 @@ module.exports = (() => {
 	const alertStoppedAll = new EventType('ALERT-STOPPED-ALL', 'Alert Stopped', ProductType.ALERT, ['userId']);
 	const alertNotesEdited = new EventType('ALERT-NOTES-EDITED', 'Alert Notes Edited', ProductType.ALERT, ['userId', 'alertId']);
 	const alertDeleted = new EventType('ALERT-DELETED', 'Alert Deleted', ProductType.ALERT, ['userId', 'alertId']);
-	const alertDeletedAll = new EventType('ALERT-DELETED-ALL', 'Alert Deleted', ProductType.ALERT, ['userId', 'alertId']);
+	const alertDeletedAll = new EventType('ALERT-DELETED-ALL', 'Alert Deleted', ProductType.ALERT, ['userId']);
 
 	// Watchlist
 
